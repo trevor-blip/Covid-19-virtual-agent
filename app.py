033b903478ce9a0a645ec9c7752eda52349e7805
Trevor Gandanhamo <gandanhamotrevor@gmail.com>
2020-08-16 09:20:45 +0200

    make it better

--- app.py
+++ app.py
@@ -314,5 +314,9 @@ def prepareEmail(contact_list):
     mailclient.sendEmail(contact_list)
 
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)  # running the app on the local machine on port 5000
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+'''if __name__ == "__main__":
+    app.run(port=5000, debug=True)''' # running the app on the local machine on port 8000
